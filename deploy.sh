5e9b4c45d27e47eb8e21f3a7c593d5ad28cb98b7
Aris Ripandi <ar.is@outlook.com>
2018-04-07 21:40:59 +0700

    Change index

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+if [[ ! -f "/usr/bin/minify" ]]; then
+    sudo wget -qO- github.com/tdewolff/minify/releases/download/v2.3.4/minify_2.3.4_linux_amd64.tar.gz | sudo tar zxv -C /tmp
+    sudo mv /tmp/minify /usr/bin/minify
+    sudo chmod +x /usr/bin/minify
+fi
+
 read -p "Commit description: " desc
 git add . && \
 git add -u && \
 git commit -m "$desc" && \
 git push origin master
+
