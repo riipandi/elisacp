c2ad7cdabb2c818f57b27365fd273fced0f10a36
Aris Ripandi <ar.is@outlook.com>
2018-04-07 21:52:18 +0700

    Minify HTML Output

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [[ ! -f "/usr/bin/minify" ]]; then
     sudo chmod +x /usr/bin/minify
 fi
 
-minify -r -v --match=\.html public/
+minify -r --match=\.html -o public/ public
 
 read -p "Commit description: " desc
 git add . && \
