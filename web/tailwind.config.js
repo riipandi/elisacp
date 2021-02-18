d2a5d7e79e733c88006afe74b9e6463992c1bf0e
Aris Ripandi <ar.is@outlook.com>
2021-02-18 22:27:58 +0700

    Initialize TailwindUI for web frontend

---
+++ web/tailwind.config.js
@@ -0,0 +1,46 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultColor = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Gellix', ...defaultTheme.fontFamily.sans]
+      },
+      screens: {
+        standalone: { raw: '(display-mode: standalone)' }
+      },
+      colors: {
+        gray: defaultColor.trueGray,
+        primary: defaultColor.rose,
+        secondary: defaultColor.orange,
+        accent: defaultColor.indigo
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: theme('colors.primary.600')
+              }
+            }
+          }
+        }
+      })
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    // Additional first-party plugins
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp')
+  ]
+}
