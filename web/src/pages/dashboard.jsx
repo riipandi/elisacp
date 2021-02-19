c9f7f9ce3ac259cbe2e34529ed969d5e49d80a9e
Aris Ripandi <ar.is@outlook.com>
2021-02-19 07:29:35 +0700

    Use page loader in dashboard

--- web/src/pages/dashboard.jsx
+++ web/src/pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import DefaultLayout from '../layouts/default'
-
+import PageLoader from '../components/page-loader'
 import { userService, authenticationService } from '../services'
 
 class Dashboard extends Component {
@@ -8,25 +8,37 @@ class Dashboard extends Component {
     super(props)
     this.state = {
       currentUser: authenticationService.currentUserValue,
+      loading: true,
       users: null
     }
   }
 
+  async loadData() {
+    setTimeout(() => {
+      userService.getAll().then((users) => this.setState({
+        loading: false,
+        users
+      }))
+    }, 800)
+  }
+
   componentDidMount() {
-    userService.getAll().then((users) => this.setState({ users }))
+    this.loadData()
   }
 
   render() {
-    const { currentUser, users } = this.state
+    const { currentUser, loading, users } = this.state
 
     return (
-      <DefaultLayout>
-        <h2>Dashboard</h2>
-        <div>
-          <h1>Hi {currentUser.name}!</h1>
-          <p>You're logged in with React &amp; JWT!!</p>
-          <h3>Users from secure api end point:</h3>
-          {/* {users && (
+      <>
+        <PageLoader loading={loading} />
+        <DefaultLayout title="Dashboard">
+          <h2>Dashboard</h2>
+          <div>
+            <h1>Hi {currentUser.name}!</h1>
+            <p>You're logged in with React &amp; JWT!!</p>
+            <h3>Users from secure api end point:</h3>
+            {/* {users && (
             <ul>
               {users.map((user) => (
                 <li key="{user.ID}">
@@ -35,8 +47,9 @@ class Dashboard extends Component {
               ))}
             </ul>
           )} */}
-        </div>
-      </DefaultLayout>
+          </div>
+        </DefaultLayout>
+      </>
     )
   }
 }
